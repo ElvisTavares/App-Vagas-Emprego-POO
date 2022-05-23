28f95dfa418a80f432c8de0ef3b9d60f7281b64c
elvis <elvisptavares@gmail.com>
2022-05-22 21:38:14 -0300

    Organização de arquivos e layout

---
+++ includes/formulario.php
@@ -0,0 +1,13 @@
+<main>
+    <section>
+        <a href="index.php">
+            <button class="btn btn-success">Voltar</button>
+        </a>
+    </section>
+
+    <h2 class="mt-3">Cadastrar vaga</h2>
+
+    <form action="">
+        
+    </form>
+</main>
